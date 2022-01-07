f3ca02091484b6e5b8c18222b620164bcaef853e
Aaytee32 <aopponggyebi@yahoo.com>
2022-01-07 17:43:03 -0500

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ## 
 golem::fill_desc(
   pkg_name = "OrximTechPOS", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "OrximTech Inventory System", # The Title of the package containing the App 
+  pkg_description = "Pharmacy Point of Sale, Inventory and Analytics System by OrximTech", # The Description of the package containing the App 
+  author_first_name = "Anthony", # Your First Name
+  author_last_name = "Oppong-Gyebi", # Your Last Name
+  author_email = "aaytee20008@gmail.com", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
